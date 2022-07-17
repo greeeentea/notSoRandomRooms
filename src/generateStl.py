fd466a6d45a80737ece020d8ade93f71460d31ff
greeeentea <marionamberlorenz@googlemail.com>
2022-07-17 21:02:53 +0200

    command line flags

--- src/generateStl.py
+++ src/generateStl.py
@@ -1,14 +1,23 @@
 #!/bin/python3
-
+import argparse
 import rrooms.rrooms as rms
 
 
 if __name__ == "__main__":
-    
-    times = int(input("Number of random rooms:"))
-    path = input("path to output-folder")
-    numberOfObjects = int(input("Number of random objects:"))
 
-    for x in range(times):
-        room = rms.room(numberOfObjects)
-        room.writeToStl(path,str(x).zfill(4)+ 'von'+str(times)+'_nO'+str(numberOfObjects))
+    parser = argparse.ArgumentParser(description="Generates r stl-files with o random objects in it. ",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "-r", "--rooms",  help="the number of rooms", required=True, type=int)
+    parser.add_argument(
+        "-p", "--path",  help="the path for stl-files", required=True)
+    parser.add_argument("-o", "--objects",
+                        help="the number of objects", required=True, type=int)
+
+    args = parser.parse_args()
+    config = vars(args)
+
+    for x in range(config['rooms']):
+        room = rms.room(config['objects'])
+        room.writeToStl(config['path'], str(x).zfill(
+            4) + 'von'+str(config['rooms'])+'_nO'+str(config['objects']))
