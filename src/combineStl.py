abae462ff380bfb11606f5bab956879de8885643
greeeentea <marionamberlorenz@googlemail.com>
2022-09-24 20:18:04 +0200

    all files in folder 1

--- src/combineStl.py
+++ src/combineStl.py
@@ -1,8 +1,9 @@
 import argparse
+from operator import truediv
+from tkinter import StringVar
 import numpy
 import stl
 import os
-import random
 from stl import mesh
 
 
@@ -20,18 +21,28 @@ if __name__ == '__main__':
         '-d', '--destination',  help='destination for saving the combined stl', required=True)
     parser.add_argument(
         '-f1n', '--f1name',  help='full name of f1-file in output-file-name', action='store_true')
+    parser.add_argument(
+        '-f1all', '--allfirst',  help='all the files of the first folder', action='store_true')
 
     config = vars(parser.parse_args())
 
     for n in range(config['ntimes']):
-        randomFile1 = random.choice(os.listdir(config['folder1']))
-        randomFile2 = random.choice(os.listdir(config['folder2']))
-
+        randomFile1 ="";
+        
+        firstFolder = os.listdir(config['folder1']);
+        secondFolder = os.listdir(config['folder2']);
+        
+        if(config['allfirst']):
+            randomFile1 = firstFolder[n%len(firstFolder)]
+        else:
+            randomFile1 = numpy.random.choice(os.listdir(config['folder1']))
+        randomFile2 = numpy.random.choice(secondFolder,replace=True)
+        
         mesh1 = mesh.Mesh.from_file(config['folder1']+randomFile1)
         mesh2 = mesh.Mesh.from_file(config['folder2']+randomFile2)
 
         combinedFileName = randomFile1[len(
-        randomFile1)-25:len(randomFile1)-4]+randomFile1[len(randomFile2)-25:len(randomFile2)]
+        randomFile1)-25:len(randomFile1)-4]+randomFile2[len(randomFile2)-25:len(randomFile2)]
         
         if(config['f1name']):
             combinedFileName = randomFile1[0:len(randomFile1)-4]+randomFile1[len(randomFile2)-25:len(randomFile2)]
